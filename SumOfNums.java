565012773bd6119858ba80cd6fe6d7c89bcfcb0a
Ravulapalliajay <ajayravulapalli77@gmail.com>
2024-08-03 09:08:23 +0530

    Add files via upload

---
+++ SumOfNums.java
@@ -0,0 +1,21 @@
+package com.example.sumofnum;
+import java.util.Scanner;
+
+public class SumOfNums {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+
+        do {
+            System.out.print("Enter a number: ");
+            int num = scanner.nextInt();
+            sum += num;
+            count++;
+        } while (count < 10);
+
+        System.out.println("The sum of the numbers is: " + sum);
+    }
+}
+
